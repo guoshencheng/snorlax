584150289a37fb7b840a48b0999271719b873cb6
guoshencheng <648772021@qq.com>
2018-02-20 22:36:11 +0800

    add update and create

--- controllers/v1/postCategory.js
+++ controllers/v1/postCategory.js
@@ -19,6 +19,45 @@ const findById = async (req, res, next) => {
   }
 }
 
+const create = async (req, res, next) => {
+  const body = req.body;
+  const attributes = Object.keys(db.PostCategory.attributes);
+  const params = {};
+  Object.keys(params).forEach(key => {
+    if (body[key]) {
+      params[key] = body[key]
+    }
+  })
+  try {
+    const postCategory = await db.PostCategory.create(params);
+    res.makeJson(postCategory.toJSON());
+  } catch (e) {
+    next(e);
+  }
+}
+
+const update = async (req, res, next) => {
+  const id = req.params.id;
+  const body = req.body;
+  const attributes = Object.keys(db.PostCategory.attributes);
+  try {
+    var postCategory = await db.PostCategory.findById(id);
+    if (!postCategory) {
+      throw new Error(`post category id ${id} not found`);
+      return;
+    }
+    Object.keys(params).forEach(key => {
+      if (body[key]) {
+        postCategory[key] = body[key]
+      }
+    })
+    postCategory = await postCategory.save();
+    res.makeJson(postCategory.toJSON())
+  } catch (e) {
+    next(e)
+  }
+}
+
 module.exports = {
-  all, findById
+  all, findById, create, update
 };
