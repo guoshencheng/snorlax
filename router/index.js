3eb04c2bea364b46e5dd63d1a50e9ab20f9466b6
guoshencheng <648772021@qq.com>
2018-08-16 23:47:56 +0800

    try to use apollo

--- router/index.js
+++ router/index.js
@@ -1,12 +1,24 @@
-const Router = require('koa-router');
-const router = new Router();
-const MyGraphQLSchema = require('../GraphqlSchema');
+const Koa = require('koa');
+const { ApolloServer, gql } = require('apollo-server-koa');
+import {   } from '@mhc/mum-components';
 
-const graphqlHTTP = require('koa-graphql');
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-router.all('/graphql', graphqlHTTP({
-  schema: MyGraphQLSchema,
-  graphiql: true
-}));
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
 
-module.exports = router;
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = new Koa();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+);
