baf438700a793603c259e4dc0f29244c35b02ebb
guoshencheng <648772021@qq.com>
2018-08-17 10:23:21 +0800

    add nodemon

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const Koa = require('koa');
 const app = new Koa();
-const router = require('./router');
 
-app.use(router.routes()).use(router.allowedMethods());
+const { ApolloServer, gql } = require('apollo-server-koa');
+const schema = require('./GraphqlSchema');
+
+const server = new ApolloServer({ schema });
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+);
 
 module.exports = app;
